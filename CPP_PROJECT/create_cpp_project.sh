c7da445e32b0dfd0356f7f9c169cc07ae161354b
DodoT <dorian_thivolle@orange.fr>
2017-09-04 23:12:19 +0200

    Base Commit

---
+++ CPP_PROJECT/create_cpp_project.sh
@@ -0,0 +1,38 @@
+
+# Create C++ project
+
+#!/bin/bash
+
+if [ "$1" == "-p" ] &&  [ ! -z $2 ] &&  [ -d $2 ]
+then
+       	echo "[i] Chemin : $2"
+
+	read -p "Entrer le nom du projet : " project
+
+	if [ ! -z $project ]
+	then
+		DIR=$2
+		END=${#DIR}-1
+
+		if [ "${DIR:END}" != "/" ]; then
+  			echo "[i] last character is ${DIR:END} adding slash."
+  			DIR=$2"/"
+		else
+			echo "[v] valid path."
+		fi
+		echo $DIR
+		mkdir $DIR$project
+		mkdir $DIR$project"/apps/"
+		mkdir $DIR$project"/apps/your_app/"
+		mkdir $DIR$project"/tests/"
+		mkdir $DIR$project"/src/"
+		cp ./util/Main_Template.cpp $DIR$project"/src/main.cpp"
+		cp ./util/Main_Template.cpp $DIR$project"/tests/main_test.cpp"
+		cp ./util/Makefile_Template $DIR$project"/Makefile"
+		ls $DIR$project"/"
+	else
+		echo "[x] Erreur dans le nom du projet."
+	fi
+else
+       	echo "[x] Erreur dans le chemin."
+fi
